95c49150cf5e4d91522baca016d4d35e9f52c1b1
vidaNdecky <david.ndecky@finetechgroupe.com>
2022-12-07 17:24:58 +0000

    first commit

--- src/main/java/sn/isi/bankcredit/controller/CompteController.java
+++ src/main/java/sn/isi/bankcredit/controller/CompteController.java
@@ -1,4 +1,19 @@
 package sn.isi.bankcredit.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/comptes")
 public class CompteController {
+    @GetMapping("/comptes-list")
+    public String list() {
+        return "comptes/comptesList";
+    }
+
+    @GetMapping("/add-compte")
+    public String add() {
+        return "comptes/addCompte";
+    }
 }
--- src/main/java/sn/isi/bankcredit/controller/UserController.java
+++ src/main/java/sn/isi/bankcredit/controller/UserController.java
@@ -1,4 +1,19 @@
 package sn.isi.bankcredit.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/users")
 public class UserController {
+    @GetMapping("/users-list")
+    public String list() {
+        return "users/usersList";
+    }
+
+    @GetMapping("/add-user")
+    public String add() {
+        return "users/addUser";
+    }
 }
